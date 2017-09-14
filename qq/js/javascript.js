5b91e3f56f47549f5a80cd0c98c766a551ddf493
869831540 <869831540@qq.com>
2017-09-14 13:52:52 +0800

    ssl

---
+++ qq/js/javascript.js
@@ -0,0 +1,19 @@
+$(function(){
+	$('#navigation').hover(function(){
+		$(".navexpan").css({'display':'block'});
+	},function(){
+		$(".navexpan").css({'display':'none'});
+
+	})
+	
+	$('.navexpan').hover(function(){
+		$(".navexpan").css({'display':'block'});
+	},function(){
+		$(".navexpan").css({'display':'none'});
+	});
+	
+	
+	
+	
+	
+})
